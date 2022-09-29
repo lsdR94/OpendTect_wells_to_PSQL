b6da321b0381e7f65fa1a6dbfc52f56d87f0c9f4
lsdR94 <luisdarc@gmail.com>
2022-09-29 15:58:32 -0400

    feat: wells_table_creation method

--- py_to_psql.py
+++ py_to_psql.py
@@ -40,3 +40,51 @@ def execute_psql_command(command, connection):
         print("\n".join(details))
         print(e.__class__.__name__, ":", e)
         print(f"Command can not be processed. Execution time = {end - init}s")
+        
+def wells_table_creation(table_name, connection, column_list=[]):
+    """
+    Creates psql tables based on OPENDTECT wells info.
+    
+    PARANETERS
+    ----------
+        table_name : str
+            PostgreSQL table to create.
+        
+        connection : psycopg2.extensions.connection
+            Parameters to create a connection between end user
+            and PSQL server.
+            
+        column_list : list (optional)
+            List of PSQL statements for a more flexible table
+            creation.
+    
+    RETURN
+    ------
+        PSQL table. The default table is created using the 
+        following columns:
+            - well_id SERIAL NOT NULL UNIQUE PRIMARY KEY
+            - opendtect_id NUMERIC(10,1) NOT NULL
+            - well_name VARCHAR(30) NOT NULL UNIQUE
+            - x_coordinate NUMERIC(10,2) NOT NULL
+            - y_coordinate NUMERIC(10,2) NOT NULL
+            - status VARCHAR(30) NOT NULL
+    """
+    
+    if len(column_list) != 0:
+        table_creation_query = f"CREATE TABLE {table_name}("
+        for column in column_list:
+            table_creation_query += column
+    
+        table_creation_query += ")"   
+    else:
+        table_creation_query = f"""
+            CREATE TABLE {table_name}(
+                well_id SERIAL NOT NULL UNIQUE PRIMARY KEY,
+                opendtect_id NUMERIC(10,1) NOT NULL,
+                well_name VARCHAR(30) NOT NULL UNIQUE,
+                x_coordinate NUMERIC(10,2) NOT NULL,
+                y_coordinate NUMERIC(10,2) NOT NULL,
+                status VARCHAR(30) NOT NULL
+            )
+            """
+    return(execute_psql_command(table_creation_query, connection))
